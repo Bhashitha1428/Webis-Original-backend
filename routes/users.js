c727ed33605ebd311400a71db462dc04ef9d938b
bhashitha1428 <bhashithamiyurantha.bmr@gmail.com>
2019-08-28 15:18:22 +0530

    user route

--- routes/users.js
+++ routes/users.js
@@ -155,10 +155,7 @@ router.get('/allUserDetails',(req,res)=>{
     User
       .find()
       .then(users=>{
-          res.status(200).json({
-              Users:users,
-              state:true
-          })
+          res.status(200).json(users);
       })
       .catch(err=>{
        res.status(500).json({
